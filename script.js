4606af19a84b95eba8cbf819f08fbdd3df904918
Chris Johnson <chrisjohnson112@yahoo.com>
2020-01-03 18:59:04 -0600

    add file

---
+++ script.js
@@ -0,0 +1,12 @@
+let scriptText = document.getElementById('text');
+let scriptText2 = document.getElementById('text2');
+let scriptText3 = document.getElementById('text3');
+
+scriptText.innerHTML = "Success has never been easier to finance";
+
+$(function(){
+    $("#text").slideDown(500).fadeIn();
+    $("#text3").slideDown(1000).fadeIn(1000);
+    
+});
+
